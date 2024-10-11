31a4aab83f39be629145ba3b12fde6e6e380e6a3
Mirko Renzetti <renzetti@info-click.it>
2024-10-11 15:07:09 +0200

    added configurations via environment variables

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Adminer container with Pematon Theme
 Based on [Adminer (Pematon version)](https://github.com/pematon/adminer) with preinstalled [Pematon Theme](https://github.com/pematon/adminer-theme)
 ## Environment variables
-### APP_ENV
+### ADMINER_ENV
 - `local` => green theme
 - `dev`  `test`  `stage`  `staging` => blue theme
 - `prod`  `production` => orange theme (default)
 ### ADMINER_DEFAULT_SERVER
-- default server host
+Set the default server host
+### ADMINER_DEFAULT_DRIVER
+Set the default database driver
+- mysql
+- sqlite
+- sqlite2
+- pgsql
+- oracle
+- mssql
+- mongo
+- elastic
--- index.php
+++ index.php
@@ -1,35 +1,77 @@
 <?php
 
-function adminer_object()
-{
-    // Required to run any plugin.
-    include_once "./plugins/plugin.php";
+namespace docker {
+    function adminer_object()
+    {
+        require_once('plugins/plugin.php');
 
-    // Plugins auto-loader.
-    foreach (glob("plugins/*.php") as $filename) {
-        include_once "./$filename";
+        class Adminer extends \AdminerPlugin
+        {
+            function _callParent($function, $args)
+            {
+                if ($function === 'loginForm') {
+                    ob_start();
+                    $return = \Adminer::loginForm();
+                    $form = ob_get_clean();
+
+                    // Set default server
+                    $form = str_replace('name="auth[server]" value="" title="hostname[:port]"', 'name="auth[server]" value="' . getenv('ADMINER_DEFAULT_SERVER') . '" title="hostname[:port]"', $form);
+
+                    // Set default drive
+                    $driver = getenv('ADMINER_DEFAULT_DRIVER') ?? 'server';
+                    $driver = $driver === 'mysql' ? 'server' : $driver;
+
+                    $form = str_replace(' selected>', '>', $form);
+                    $form = str_replace('value="' . $driver . '">', 'value="' . $driver . '" selected>', $form);
+
+                    echo $form;
+
+                    return $return;
+                }
+
+                return parent::_callParent($function, $args);
+            }
+        }
+
+        $plugins = [];
+
+        if (!empty(getenv('ADMINER_DESIGN'))) {
+            # compatibility with previous custom version
+            $pematon_theme = getenv('ADMINER_DESIGN');
+        } else {
+            $pematon_theme = match (getenv('ADMINER_ENV')) {
+                'local' => 'default-green',
+                'dev', 'test', 'stage', 'staging' => 'default-blue',
+                'prod', 'production' => 'default-orange',
+                default => 'default-orange',
+            };
+        }
+
+        require_once('plugins/AdminerTheme.php');
+        $plugins[] = new \AdminerTheme($pematon_theme);
+
+        foreach (glob('plugins/*.php') as $plugin) {
+            if (in_array(basename($plugin), ['AdminerTheme.php', 'plugin.php'])) {
+                continue;
+            }
+            $plugins[] = require($plugin);
+        }
+
+        return new Adminer($plugins);
     }
-
-    if (!empty(getenv('ADMINER_DESIGN'))) {
-        # compatibility with previous custom version
-        $pematon_theme = getenv('ADMINER_DESIGN');
-    } else {
-        $pematon_theme = match (getenv('APP_ENV')) {
-            'local' => 'default-green',
-            'dev', 'test', 'stage', 'staging' => 'default-blue',
-            'prod', 'production' => 'default-orange',
-            default => 'default-orange',
-        };
-    }
-
-    // Specify enabled plugins here.
-    $plugins = [
-        // AdminerTheme has to be the last one!
-        new AdminerTheme($pematon_theme),
-    ];
-
-    return new AdminerPlugin($plugins);
 }
 
-// Include original Adminer or Adminer Editor.
-include "./adminer.php";
+namespace {
+    if (basename($_SERVER['DOCUMENT_URI'] ?? $_SERVER['REQUEST_URI']) === 'adminer.css' && is_readable('adminer.css')) {
+        header('Content-Type: text/css');
+        readfile('adminer.css');
+        exit;
+    }
+
+    function adminer_object()
+    {
+        return \docker\adminer_object();
+    }
+
+    require('adminer.php');
+}
